bfcf618e0e435b70a879270850127a867622bf83
errolgh <ewatson@ggoutfitters.com>
2020-12-25 01:15:44 -0500

    added graphql layer

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,11 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'Full-stack Bootcamp',
+    author: 'Andrew Mead',
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+const ContactPage = () => {
+    return(
+        <Layout>
+            <h1>Contact</h1>
+            <p>The best way to reach me is via <a rel="noreferrer" target="_blank" href="https://twitter.com/andrew_j_mead?lang=en">Twitter!</a></p>
+        </Layout>
+    )
+}
+
+export default ContactPage
